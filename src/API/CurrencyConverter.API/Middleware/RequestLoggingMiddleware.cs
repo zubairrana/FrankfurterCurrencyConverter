965d88cca6d5f6fe661c17e73b2eecf50cf4e6a7
Zubair <zubair@mydom.com>
2026-03-29 18:09:03 +0400

    ClientId added in log message

--- src/API/CurrencyConverter.API/Middleware/RequestLoggingMiddleware.cs
+++ src/API/CurrencyConverter.API/Middleware/RequestLoggingMiddleware.cs
@@ -10,18 +10,23 @@ namespace CurrencyConverter.API.Middleware
         {
             var stopwatch = Stopwatch.StartNew();
             var clientIp = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+            var clientId = context.User?.Identity?.IsAuthenticated == true
+                ? context.User.FindFirst("client_id")?.Value
+                ?? context.User.FindFirst("sub")?.Value
+                : null;
 
             await next(context);
 
             stopwatch.Stop();
 
             logger.LogInformation(
-                "HTTP {Method} {Path} responded {StatusCode} in {ElapsedTimeSpan} | ClientIP: {ClientIP}",
+                "HTTP {Method} {Path} responded {StatusCode} in {ElapsedTimeSpan} | ClientIP: {ClientIP} | ClientId: {ClientId}",
                 context.Request.Method,
                 context.Request.Path,
                 context.Response.StatusCode,
                 TimeSpan.FromMicroseconds(stopwatch.ElapsedMilliseconds),
-                clientIp);
+                clientIp,
+                clientId);
         }
     }
 }
